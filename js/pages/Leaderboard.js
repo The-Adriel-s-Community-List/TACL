3495731d97f2e7faa49097593c33642cbe6c2ea9
noskillGMD <playgames3066pro@gmail.com>
2026-07-14 21:19:22 -0300

    Update Leaderboard.js

--- js/pages/Leaderboard.js
+++ js/pages/Leaderboard.js
@@ -53,8 +53,16 @@ export default {
                                 <p class="type-label-lg">#{{ i + 1 }}</p>
                             </td>
                             <td class="total">
-                                <p class="type-label-lg">{{ localize(ientry.total) }}</p>
-                            </td>
+    <div class="cp-total">
+        <span class="type-label-lg">{{ localize(ientry.total) }}</span>
+
+        <img
+            src="/images/creator-point.png"
+            alt="CP"
+            class="cp-icon"
+        >
+    </div>
+</td>
                             <td class="user" :class="{ 'active': selected == i }">
                                 <button @click="selected = i">
                                     <span class="type-label-lg">{{ ientry.user || ientry.creator }}</span>
